ef3e5da5b331e1da2be7f2e9b4e7f95370593d82
takeshi <takagitakeshi2@gmail.com>
2019-07-07 23:33:30 +0900

    first commit

---
+++ src/main/java/com/example/dateCalculation/DateCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dateCalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateCalculationApplication.class, args);
+	}
+
+}
